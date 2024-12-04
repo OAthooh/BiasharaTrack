0507f9401de2f30a9b94852622adcd9725e9299b
weakinyi <akinyiwendy46@gmail.com>
2024-12-04 12:01:01 +0300

    feat: add floating language switcher file

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,17 @@
-
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Login from './pages/Login';
 import SignUp from './pages/SignUp';
 import Dashboard from './pages/Dashboard';
 import ProtectedRoute from './context/ProtectedRoute';
+import FloatingLanguageSwitcher from './components/FloatingLanguageSwitcher';
 
 function App() {
   return (
     <Router>
+      <FloatingLanguageSwitcher />
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<Home /> } />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<SignUp />} />
         <Route
