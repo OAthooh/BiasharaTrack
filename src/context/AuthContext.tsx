d29bb18f9a8b1e71d4450d5b70db032431e4539c
oathooh <oathooh@gmail.com>
2024-12-06 16:32:24 +0300

    update: added authContext fields for user authentication

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -1,13 +1,10 @@
 import { createContext, useContext, ReactNode, useEffect, useState } from 'react';
 import { useAuth as useAuthHook } from '../utils/auth';
+import { User } from '../types/user';  // Import the User interface
 
 interface AuthContextType {
   isAuthenticated: boolean;
-  user: {
-    id: number;
-    fullName: string;
-    email: string;
-  } | null;
+  user: User | null;
   loading: boolean;
   error: string | null;
   login: (credentials: { email: string; password: string }) => Promise<void>;
@@ -32,11 +29,11 @@ export const AuthProvider = ({ children }: { children: ReactNode }) => {
 
   useEffect(() => {
     const initAuth = async () => {
-      checkAuth();
+      await checkAuth();
       setInitialized(true);
     };
     initAuth();
-  }, []);
+  }, [checkAuth]);
 
   if (!initialized || loading) {
     return <div>Loading...</div>;
@@ -47,10 +44,13 @@ export const AuthProvider = ({ children }: { children: ReactNode }) => {
       value={{ 
         isAuthenticated: !!user,
         user: user ? {
-          id: user.id,
-          fullName: user.full_name || '',
-          email: user.email
-        } : null,
+          id: String(user.id),
+          name: user.full_name || '',
+          email: user.email,
+          role: 'owner',
+          businessName: 'Default Business', // businessName: user.business_name || 'Default Business', implement real busines name
+          imageUrl: ''
+          } : null,
         loading,
         error,
         login,
@@ -69,6 +69,5 @@ export const useAuth = () => {
   if (!context) {
     throw new Error('useAuth must be used within an AuthProvider');
   }
-  console.log("Context", context);
   return context;
 };
